5fa04f06555bbb043217db77e370639361b7be06
Mitva <115395431+MitvaVirvadiya@users.noreply.github.com>
2023-10-27 21:35:36 +0530

    Todolist with redux

---
+++ 11reduxTodo/src/App.jsx
@@ -0,0 +1,17 @@
+import { useEffect, useState } from 'react'
+import TodoForm from './components/todoForm'
+import TodoList from './components/todoList'
+
+function App() {
+
+  return (
+    <>
+      <div className='w-full max-w-2xl bg-green-400 mx-auto my-4 px-4 py-6 rounded-lg shadow-lg flex flex-wrap justify-center items-center'>
+        <TodoForm />
+        <TodoList />
+      </div>
+    </>
+  )
+}
+
+export default App
